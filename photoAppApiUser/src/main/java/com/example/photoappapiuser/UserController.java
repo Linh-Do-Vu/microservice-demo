dbb9d5a9f0a9ec98eb751ca2a07e7bdb78bfae53
LinhDoVu <vulinh102ii@gmail.com>
2023-12-14 17:13:28 +0700

    fix api gateway

--- photoAppApiUser/src/main/java/com/example/photoappapiuser/UserController.java
+++ photoAppApiUser/src/main/java/com/example/photoappapiuser/UserController.java
@@ -5,17 +5,19 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@CrossOrigin("*")
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private Environment env ;
+//    @Autowired
+//    private Environment env ;
     @GetMapping(value = "/check")
     public ResponseEntity<?> showString () {
-        return new ResponseEntity<>("work" + env.getProperty("local.server.port"), HttpStatus.ACCEPTED);
+        return new ResponseEntity<>("work" , HttpStatus.ACCEPTED);
     }
 }
--- spring_api_gateway/src/main/java/com/example/spring_api_gateway/SpringApiGatewayApplication.java
+++ spring_api_gateway/src/main/java/com/example/spring_api_gateway/SpringApiGatewayApplication.java
@@ -2,10 +2,9 @@ package com.example.spring_api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
-
 public class SpringApiGatewayApplication {
 
 	public static void main(String[] args) {
